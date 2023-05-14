e072a96ecb23ae13ac7824abc149d1f476fe74e4
haowy-design <116164296+haowy-design@users.noreply.github.com>
2023-05-14 21:56:07 +1000

    Version 1.1
    Added animations via JQuery and edited HTML & CSS

---
+++ java/index.js
@@ -0,0 +1,50 @@
+$(document).ready(function(){
+
+    //Loading Screen
+    setTimeout(function(){
+        $("#nameLogo").animate({left:"+=30%"}, 900);
+        $("#loading-screen").fadeOut(850);
+    }, 3500);
+
+    //Page Links
+
+    //Graphic Link
+    $("#graphicText").hover(function(){
+        $("body").addClass("change-background");
+        $(".descriptionArea,.nameArea > p").addClass("change-element-color");
+        $(".hpbutton").addClass("inverted-button-colors");
+        $(".nameArea").addClass("graphicImage");
+    }, function(){
+        $("body").removeClass("change-background");
+        $(".descriptionArea,.nameArea > p").removeClass("change-element-color");
+        $(".hpbutton").removeClass("inverted-button-colors");
+        $(".nameArea").removeClass("graphicImage");
+    });
+
+    //UX/UI Link
+    $("#uxText").hover(function(){
+        $("body").addClass("change-background");
+        $(".descriptionArea,.nameArea > p").addClass("change-element-color");
+        $(".hpbutton").addClass("inverted-button-colors");
+        $(".nameArea").addClass("uxImage");
+    }, function(){
+        $("body").removeClass("change-background");
+        $(".descriptionArea,.nameArea > p").removeClass("change-element-color");
+        $(".hpbutton").removeClass("inverted-button-colors");
+        $(".nameArea").removeClass("uxImage");
+    });
+
+    //Designer Link
+    $("#designerText").hover(function(){
+        $("body").addClass("change-background");
+        $(".descriptionArea,.nameArea > p").addClass("change-element-color");
+        $(".hpbutton").addClass("inverted-button-colors");
+        $(".nameArea").addClass("designerImage");
+    }, function(){
+        $("body").removeClass("change-background");
+        $(".descriptionArea,.nameArea > p").removeClass("change-element-color");
+        $(".hpbutton").removeClass("inverted-button-colors");
+        $(".nameArea").removeClass("designerImage");
+    });
+    
+});
